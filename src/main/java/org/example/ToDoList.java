fec167d34b0ec42d8965dfcb3fd38154a7bf7068
NataliaSA75 <natashagolub9@gmail.com>
2025-11-27 18:12:06 +0700

    add new functions

--- src/main/java/org/example/ToDoList.java
+++ src/main/java/org/example/ToDoList.java
@@ -1,6 +1,8 @@
 package org.example;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 public class ToDoList {
     private final List<String> items = new ArrayList<>();
@@ -21,6 +23,29 @@ public class ToDoList {
     }
     public List<String> getAll() {
         return new ArrayList<>(items);
+    }
+
+    public boolean clear() {
+        this.items.clear();
+
+        return items.isEmpty();
+    }
+
+    public String done(int index) {
+        String task = items.get(index);
+
+        task = task + " -- completed";
+        items.set(index, task);
+
+        return items.get(index);
+    }
+
+    public Set<String> search(String substring) {
+        return items.stream()
+                .filter(
+                        x -> x.contains(substring)
+                )
+                .collect( Collectors.toSet() );
     }
     public int size() {
         return items.size();
