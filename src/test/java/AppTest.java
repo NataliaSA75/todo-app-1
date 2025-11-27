3f0c5ff4e2a8eeeca6cdf32bffd5587b3b3270fa
NataliaSA75 <natashagolub9@gmail.com>
2025-11-27 18:16:13 +0700

    add tests

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -1,5 +1,8 @@
 import org.example.ToDoList;
 import org.junit.jupiter.api.Test;
+
+import java.util.Set;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -25,5 +28,32 @@ public class AppTest {
         ToDoList t = new ToDoList();
         t.add(" ");
         assertEquals(0, t.size());
+    }
+
+    @Test
+    void clear() {
+        ToDoList t = new ToDoList();
+        t.add("task1");
+        t.add("task2");
+        assertTrue(t.clear());
+        assertEquals(0, t.size());
+    }
+
+    @Test
+    void done() {
+        ToDoList t = new ToDoList();
+        t.add("Buy milk");
+        String result = t.done(0);
+        assertEquals("Buy milk -- completed", result);
+    }
+
+    @Test
+    void search() {
+        ToDoList t = new ToDoList();
+        t.add("Buy milk");
+        t.add("Read book");
+        Set<String> result = t.search("Buy");
+        assertEquals(1, result.size());
+        assertTrue(result.contains("Buy milk"));
     }
 }
